314479e32bee2392ca0040dd61641c5d2defaff4
Genialngash <ngangadanche@gmail.com>
2022-09-22 17:13:56 +0300

    file change

--- bin/main.dart
+++ bin/main.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 import 'package:pubsec_dependecy_sorter/pubsec_dependency_sorter.dart';
 
-void main() {
-  String? path = stdin.readLineSync();
-  print(path);
-  pubsecDependencySorter(args: path??"");
+void main(List<String> args) {
+  
+  
+  pubsecDependencySorter(args: args);
 }
