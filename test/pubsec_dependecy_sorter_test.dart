e4b53eeb5bd5e632d0bae34b3f3c0f04aeba0aa0
Genialngash <ngangadanche@gmail.com>
2022-09-22 13:33:44 +0300

    🔧 folder restructure

--- test/pubsec_dependecy_sorter_test.dart
+++ test/pubsec_dependecy_sorter_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pubsec_dependecy_sorter/pubsec_dependecy_sorter.dart';
+import 'package:pubsec_dependecy_sorter/pubsec_dependency_sorter.dart';
 
 void main() {
   test('adds one to input values', () {
