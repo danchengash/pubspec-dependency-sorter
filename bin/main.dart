25b35604732eeb5fd103a8d774996738b64d9bf6
Genialngash <ngangadanche@gmail.com>
2022-09-22 14:44:06 +0300

    error

--- bin/main.dart
+++ bin/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:pubsec_dependecy_sorter/pubsec_dependency_sorter.dart';
 
 void main(List<String> args) {
+  print(args);
   String path = '';
   if (args.isEmpty) {
     path = Directory.current.path;
