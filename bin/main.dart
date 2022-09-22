8b26b196d09daaea6aa1186ccf8ce38e87f5f307
Genialngash <ngangadanche@gmail.com>
2022-09-22 14:37:53 +0300

    bug fix

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:pubsec_dependecy_sorter/pubsec_dependency_sorter.dart';
 
 void main(List<String> args) {
-  final String path;
+  String path = '';
   if (args.isEmpty) {
     path = Directory.current.path;
   } else {
