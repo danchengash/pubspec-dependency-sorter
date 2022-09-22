13f5a7b0eb67883240ac4c61818d9f2bb3b6a9e1
Genialngash <ngangadanche@gmail.com>
2022-09-22 11:42:26 +0300

    first commit

---
+++ test/pubsec_dependecy_sorter_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:pubsec_dependecy_sorter/pubsec_dependecy_sorter.dart';
+
+void main() {
+  test('adds one to input values', () {
+    
+  });
+}
